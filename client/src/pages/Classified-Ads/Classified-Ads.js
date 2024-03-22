961d74f5966d502c455923791ebf238b9e65a8dc
Gurveer <45864502+Gurveer-C1331@users.noreply.github.com>
2024-03-22 17:41:47 -0400

    Implemented page header & toolbar

--- client/src/pages/Classified-Ads/Classified-Ads.js
+++ client/src/pages/Classified-Ads/Classified-Ads.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Classified-Ads.css';
+import { faSliders } from '@fortawesome/free-solid-svg-icons';
 
 function ClassifiedAds() {
 
   return (
     <div>
-      <h1>Classified Ads</h1>
+      <div id='classified-ads-heading'>
+        <h1>Classified Ads</h1>
+        <span id='page-subheading'>And a subheading describing your site, too</span>
+      </div>
+
+      <div id='classified-ads-toolbar'>
+        <div id='classified-ads-search'>
+          <input id='classified-ads-searchbar' placeholder='What are you looking for today?'></input>
+          <span id='classified-ads-search-btn'>Search</span>
+        </div>
+        <span id='classified-ads-filter-btn'>
+          <FontAwesomeIcon icon={faSliders} style={{color: "#FFF"}} />
+        </span>
+        <div id='classified-ads-filter-options' style={{display: 'none'}}></div>
+      </div>
+
+      <div id='classified-ads-listing-info'></div>
+
+      <div id='classified-ads-listing-container'></div>
     </div>
   );
 }
