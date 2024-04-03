67edcb239f08635be902c8fddf02f1aed2649631
faferrei <153234923+faferrei@users.noreply.github.com>
2024-04-03 16:53:51 -0400

    Update App.js
    Imported Login + added route path

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import HomeAdmin from './pages/Home-Admin/Home-Admin';
 import ClassifiedAdsCatalog from './pages/Classified-Ads-Listings/Classified-Ads-Listings';
 import ClassifiedAdsItem from './pages/Classified-Ads-Item/Classified-Ads-Item';
 import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route exact path='/classified-ads' element={<ClassifiedAdsCatalog></ClassifiedAdsCatalog>}></Route>
         <Route exact path='/classified-ads/:listing-id' element={<ClassifiedAdsItem></ClassifiedAdsItem>}></Route>
         <Route exact path='/register' element={<Register></Register>}></Route>
+        <Route exact path='/login' element={<Login></Login>}></Route>
       </Routes>
       <Outlet></Outlet>
 
