5d4fe63ba7841054690f9539df3e579e9a251b85
faferrei <153234923+faferrei@users.noreply.github.com>
2024-04-03 23:51:40 -0400

    Update userController.js

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel');
 
 const asyncHandler = require("express-async-handler");
 
-// Register
+/*
 exports.registerUser = asyncHandler(async (req, res) => {
   const newUser = new User(req.body);
   try {
@@ -13,6 +13,46 @@ exports.registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+*/
+
+exports.registerUser = asyncHandler(async (req, res) =>
+{
+    const uname = req.params.username;
+    const fname = req.params.firstName;
+    const lname = req.params.lastName;
+    const gender = req.params.sex;
+    const addr = req.params.homeAddress;
+    const dob = req.params.DOB;
+    const phone = req.params.phoneNumber;
+    const email = req.params.email;
+    const password = req.params.password;
+    const is_admin = req.params.is_admin;
+
+    try
+    {
+        await userModel.insertOne(
+            {
+                $set:
+                {
+                    username: uname,
+                    firstName: fname,
+                    lastName: lname,
+                    sex: gender,
+                    homeAddress: addr,
+                    DOB: dob,
+                    phoneNumber: phone,
+                    email: email,
+                    password: password,
+                    is_admin: is_admin
+                }
+            })
+        return;
+    } catch (err)
+    {
+        console.log(err);
+        res.status(500).send({ error: "Error occurred adding use." });
+    }
+});
 // Login
 exports.loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
