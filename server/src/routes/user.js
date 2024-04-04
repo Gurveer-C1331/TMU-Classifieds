13a2445a242de3fe2b5a30bd0a8b005a2116ea60
faferrei <153234923+faferrei@users.noreply.github.com>
2024-04-04 12:00:12 -0400

    Update user.js

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/register/:username-:firstName-:lastName-:sex-:homeAddress-:DOB-:phoneNumber-:email-:password-:is_admin', userController.registerUser);
-router.get('/login:email-:password', userController.loginUser);
+router.get('/login/:username-:password', userController.loginUser);
 router.get('/isSignedIn', userController.isSignedIn);
 router.get('/isAdmin', userController.isAdmin);
 
